30393686a401cda625bb71a0695cf925d4d10052
José Contreras <jocontrera16@gmail.com>
2023-06-28 16:05:12 -0400

    added: clean unnecesary files

--- jenkinsfile_ms.groovy
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env groovy
-def jenkinsFile
-stage('Loading Jenkins file') {
-  jenkinsFile = fileLoader.fromGit('jf_green.groovy', 'https://github.com/AFP-Capital/jenkins-pipeline-files.git', 'generic', 'devops-github', '')
-}
