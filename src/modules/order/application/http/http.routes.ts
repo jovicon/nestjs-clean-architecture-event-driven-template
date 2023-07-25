41a544f7fb0ed41573288741a977704804d53feb
José Contreras <jocontrera16@gmail.com>
2023-07-25 00:33:31 -0400

    added: staged files && code review example tool

--- src/modules/order/application/http/http.routes.ts
+++ src/modules/order/application/http/http.routes.ts
@@ -1,7 +1,7 @@
 import { CoreModule } from './core/core.module';
 import { OrderModule } from './api/api.module';
 
-const routes = [
+const orderRoutes = [
   {
     path: '/order',
     module: CoreModule,
@@ -14,6 +14,8 @@ const routes = [
   },
 ];
 
-const routesFlat = [];
+const allRoutes = [...orderRoutes];
 
-export default routes;
+const badcode = [];
+
+export default allRoutes;
