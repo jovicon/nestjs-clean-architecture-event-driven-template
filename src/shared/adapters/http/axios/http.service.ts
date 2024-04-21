bacdbb2b0d4d90853d86b80fc82bf3b37fc942c3
José Contreras <jocontrera16@gmail.com>
2024-04-21 18:12:57 -0400

    added: post & put http actions

--- src/shared/adapters/http/axios/http.service.ts
+++ src/shared/adapters/http/axios/http.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { AxiosRequestConfig } from 'axios';
 
 @Injectable()
 export class HttpAdapterService {
   constructor(private readonly http: HttpService) {}
 
-  async get(path: string): Promise<any> {
-    return this.http.get(path);
+  async get(path: string, config: AxiosRequestConfig = {}): Promise<any> {
+    return this.http.get(path, config);
+  }
+
+  async post(path: string, body: any, config: AxiosRequestConfig = {}): Promise<any> {
+    return this.http.post(path, body, config);
+  }
+
+  async put(path: string, body: any, config: AxiosRequestConfig = {}): Promise<any> {
+    return this.http.put(path, body, config);
   }
 }
