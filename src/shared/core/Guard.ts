7af32701b6b0ce3886584b5a44eb81b93164ee71
José Contreras <jocontrera16@gmail.com>
2024-04-21 18:13:34 -0400

    added: isEmpty funtion to guard

--- src/shared/core/Guard.ts
+++ src/shared/core/Guard.ts
@@ -139,4 +139,35 @@ export class Guard {
       message: isValidRut ? '' : 'Rut invalido',
     };
   }
+
+  /**
+   * Checks if value is empty. Accepts strings, numbers, booleans, objects and arrays.
+   */
+  static isEmpty(value: unknown): boolean {
+    if (typeof value === 'number' || typeof value === 'boolean') {
+      return false;
+    }
+    if (typeof value === 'undefined' || value === null) {
+      return true;
+    }
+    if (value instanceof Date) {
+      return false;
+    }
+    if (value instanceof Object && !Object.keys(value).length) {
+      return true;
+    }
+    if (Array.isArray(value)) {
+      if (value.length === 0) {
+        return true;
+      }
+      if (value.every((item) => Guard.isEmpty(item))) {
+        return true;
+      }
+    }
+    if (value === '') {
+      return true;
+    }
+
+    return false;
+  }
 }
