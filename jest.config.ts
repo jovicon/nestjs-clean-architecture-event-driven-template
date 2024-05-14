672a0bd8230e92e8f25fe560b75384eeb940e377
José Contreras <jocontrera16@gmail.com>
2024-05-13 22:51:20 -0400

    updated: sonarqube already running

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  verbose: true,
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  testResultsProcessor: 'jest-sonar-reporter',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+    '@config/(.*)$': '<rootDir>/config/$1',
+    '^lib/(.*)$': '<rootDir>/common/$1',
+    '^@base/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/shared'],
+  coveragePathIgnorePatterns: ['<rootDir>/shared', 'node_modules', 'main.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: './',
+        outputName: 'test-reporter.xml',
+        reportedFilePath: 'absolute',
+      },
+    ],
+  ],
+};
+
+export default config;
