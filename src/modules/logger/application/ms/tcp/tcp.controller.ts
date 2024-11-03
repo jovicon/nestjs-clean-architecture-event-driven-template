6bb63953359c3fa3d0c9899f9d45302082e8c61e
jovicon <jocontrera16@gmail.com>
2024-11-02 23:47:23 -0300

    update controller by use case

--- src/modules/logger/application/ms/tcp/tcp.controller.ts
+++ src/modules/logger/application/ms/tcp/tcp.controller.ts
@@ -1,16 +1,16 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
-import { CreateLogController } from '@modules/logger/application/useCases/SendQueuesMessage/CreateLog.controller';
+import { CreateLogUseCase } from '@modules/logger/application/useCases/SendQueuesMessage/CreateLog.usecase';
 
 import { CreateLogDTO } from '@modules/logger/application/useCases/SendQueuesMessage/CreateLog.dto';
 
 @Controller()
 export class LoggerController {
-  constructor(private readonly createLogController: CreateLogController) {}
+  constructor(private readonly createLogUseCase: CreateLogUseCase) {}
 
   @MessagePattern('createdOrder')
   async executeLogger(data: CreateLogDTO): Promise<void> {
     console.log('LoggerController - executeLogger data: ', data);
-    await this.createLogController.execute(data);
+    await this.createLogUseCase.execute(data);
   }
 }
